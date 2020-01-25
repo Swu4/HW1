eb03235e4db4aaa7ec142a7b2a6b11ec07fb2616
swu4 <swu4@alaska.edu>
2020-01-24 16:25:08 -0900

    Trying to break this

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Pushing this to git, conflict2" << std::endl;
-    std::cout << "Git push for merge conflict"<<std::endl;
+    std::cout << "Pushing this to git, trying to merge back now" << std::endl;
+    std::cout << "Git push for merging to cause conflict"<<std::endl;
     return 0;
 }
