fc0b8ff55e0b98a87a629fa20863b2bd9b108668
swu4 <swu4@alaska.edu>
2020-01-24 16:10:43 -0900

    First push

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Pushing this to git" << std::endl;
+    std::cout << "Pushing this to git, this should break" << std::endl;
+    std::cout << "Git push for merge conflict"<<std::endl;
     return 0;
 }
